970a18a0addff7a93251352d8f820c9b70eafcf1
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-05-31 17:46:52 +0200

    chatserver showing the error if we exceeded 256 characters.

--- chatserver.py
+++ chatserver.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import socket
 import sys
 from _thread import *
@@ -10,6 +11,8 @@ if len(sys.argv)!=2:
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
 Info = (sys.argv[1])
 a=Info.split(':')
 IP_addr=str(a[0])
@@ -19,9 +22,10 @@ server.bind((IP_addr,port))
 server.listen(100)
 print('Hello my name is Jarvis, Welcome to the chat server')
 print('Waiting for connections')
+
+
 clients=[]
 clients.append(server)
-
 
 def clientthread(conn,addr):
     while True:
@@ -35,7 +39,7 @@ def clientthread(conn,addr):
                 conn.sendall('OK'.encode('utf-8'))
                 break
             elif len(nick1) or regex.search(nick1) != None:
-                conn.sendall('ERR malformed nick name'.encode('utf-8'))
+                conn.sendall('error malformed nick name'.encode('utf-8'))
             else:
                 conn.close()
                 print(addr[0]+" has disconnected")
@@ -50,23 +54,47 @@ def clientthread(conn,addr):
     while True:
         try:
             if conn in clients:
-                message = conn.recv(2048).decode('utf-8')
-                message1=message.strip('MSG ')
+                msg = conn.recv(2048).decode('utf-8')
+                message1=msg.strip('MSG ')
                 #print(len(message1))
             
-                if not message:
+                if not msg:
                     conn.close()
                     print(addr[0]+" has disconnected")
                     clients.remove(conn)
                     break 
 
-                else 'MSG ' not in message:
+                elif 'MSG ' not in msg:
                     conn.sendall('ERROR malformed message'.encode('utf-8'))
+                else:
+
+                
+                    if len(message1)<=255 :
+                    
+                        count=0
+                        for i in message1[:-1]:
+                            if ord(i)<31:
+                            
+                                count = count +1
+                            
+                            else:
+                                pass
+                          
+                        if count != 0:
+                            conn.sendall('ERROR -> dont use control characters'.encode('utf-8'))
+                        else:
+                            message_to_send = 'MSG '+nick1+' ' + message1[:-1]
+                
+                            broadcasting(message_to_send,conn,nick1)
+                    elif len(message1) > 255 :
+                        conn.sendall('ERROR -> message length exceeding 256 characters'.encode('utf-8'))
+                   
                 
 
         except KeyboardInterrupt:
             conn.close()
             break
+
 def broadcasting(message,connection,nick1):
     for sockets in clients:
         if sockets!= server:
@@ -84,7 +112,8 @@ while True:
     clients.append(conn)
     print(addr[0]+" has connected")
     
-
+    start_new_thread(clientthread,(conn,addr))
+ 
 
 conn.close()
-server.close()  
+server.close()   
