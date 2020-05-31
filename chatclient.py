6a9752d389e59e226ac6885124bce768392a7db0
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-05-31 16:44:25 +0200

    editing the program in order to reuse the address each and every time we restart the program and establishing connection to the server.

--- chatclient.py
+++ chatclient.py
@@ -8,13 +8,16 @@ if len(sys.argv)!=3:
     print("entry format = ./chatclient ip:port nickname")
     sys.exit()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 Info = (sys.argv[1])
 a=Info.split(':')
 IP_addr=str(a[0])
 port=int(a[1])
 nick=str(sys.argv[2])
+server.connect((IP_addr, port))
 message1=server.recv(2048).decode('utf-8')
 print(message1)
+
 nick = 'NICK ' + nick
 print('NICK '+nick.encode('utf-8'))
 server.sendall(nick.encode('utf-8'))
