853a6c48d83eaadaa61e306433c9d68cf7965c20
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-05-31 17:04:13 +0200

    showing up the error message when ever the given message is not with in the 12 given characters

--- chatserver.py
+++ chatserver.py
@@ -22,6 +22,51 @@ print('Waiting for connections')
 clients=[]
 clients.append(server)
 
+
+def clientthread(conn,addr):
+    while True:
+        
+        try:
+            nick=conn.recv(2048).decode('utf-8')
+            nick1=nick.strip('NICK ')
+            regex = re.compile('[@!#$%^&*()?/|}{~:]')
+            
+            if(regex.search(nick1) == None) and len(nick1)<=12 and 'NICK ' in nick :
+                conn.sendall('OK'.encode('utf-8'))
+                break
+            elif len(nick1) or regex.search(nick1) != None:
+                conn.sendall('ERR malformed nick name'.encode('utf-8'))
+            else:
+                conn.close()
+                print(addr[0]+" has disconnected")
+                clients.remove(conn)
+                del clients[conn]
+                break
+        except :
+                break
+
+        
+
+    while True:
+        try:
+            if conn in clients:
+                message = conn.recv(2048).decode('utf-8')
+                message1=message.strip('MSG ')
+                #print(len(message1))
+            
+                if not message:
+                    conn.close()
+                    print(addr[0]+" has disconnected")
+                    clients.remove(conn)
+                    break 
+
+                else 'MSG ' not in message:
+                    conn.sendall('ERROR malformed message'.encode('utf-8'))
+                
+
+        except KeyboardInterrupt:
+            conn.close()
+            break
 def broadcasting(message,connection,nick1):
     for sockets in clients:
         if sockets!= server:
